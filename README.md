7c01ab0c76bfed79a0474963e40a43acf348f444
GaturaN <gaturanjenga@gmail.com>
2024-07-23 12:50:20 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Tourist Boutique Insurance Cover
+
+Offers emergency deployment or transport to nearest medical facitlities to tour[A[A[B[B[B[B
+
+#### License
+
+MIT
---
+++ travelinsurance/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Tourist Boutique Insurance Cover",
+			"type": "module",
+			"label": _("Tourist Boutique Insurance Cover")
+		}
+	]
